77875bf87a18241d281a838cc86018b8093183e7
Виолетта Брюханова <vvb63@tru.ru>
2024-04-30 21:48:48 +0700

    init commit

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
-'''from dotenv import load_dotenv
-import os # будет отдавать значение переменных
-load_dotenv() # функция позволяет забрать из файлика данные
-DB_HOST = os.environ.get("DB_HOST")
-DB_PASS = os.environ.get("DB_PASS")
-DB_USER = os.environ.get("DB_USER")
-DB_NAME = os.environ.get("DB_NAME")
-DB_PORT = os.environ.get("DB_PORT")'''
 from dotenv import load_dotenv
 import os
 
