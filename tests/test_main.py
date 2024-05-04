29e405072aec05a925b40a1612446249bcee0c1a
Виолетта Брюханова <vvb63@tru.ru>
2024-05-04 15:29:57 +0700

    add test_register, test_login

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,41 +1,32 @@
-from json import dumps
-
 import pytest
-from httpx import AsyncClient
+from fastapi.testclient import TestClient
 
-from tests.conftest import client
-from conftest import client, async_session_maker
+from conftest import client
+from src.main import app
 
 def test_register():
-    user_data = {
-    "email": "pochtamy@bk.ru",
-    "password": "password",
-    "is_active": True,
-    "is_superuser": False,
-    "is_verified": False,
-    "username": "rabotay",
-    "lastname": "last",
-    "firstname": "first"
-    }
+    user_data = {"email": "testing@mail.ru", "password": "parol", "is_active": True,  "is_superuser": False,
+                 "is_verified": False, "username": "rabotay", "lastname": "last",  "firstname": "first"}
     response = client.post("/auth/register", json=user_data)
-
+    response_bad = client.post("/auth/register", json=user_data)
     assert response.status_code == 201
+    assert response.json() == { "id": 1, "email": "testing@mail.ru", "is_active": True, "is_superuser": False,
+              "is_verified": False, "username": "rabotay","lastname": "last", "firstname": "first"}
+    assert response_bad.status_code == 400
+    assert response_bad.json()["detail"] == "REGISTER_USER_ALREADY_EXISTS"
+
+def test_login():
+    response = client.post("/auth/jwt/login", data = {"username":"testing@mail.ru", "password": "parol"})
+    response_bad_login = client.post("/auth/jwt/login", data={"username": "rabotay", "password": "parol"})
+    response_bad_password = client.post("/auth/jwt/login", data={"username": "testing@mail.ru", "password": "password"})
+    response_bad_data = client.post("/auth/jwt/login", data={"username": "testing@mail.ru"})
+    assert response.status_code == 204
+    assert response_bad_login.status_code == 400
+    assert response_bad_login.json()["detail"] == "LOGIN_BAD_CREDENTIALS"
+    assert response_bad_password.status_code == 400
+    assert response_bad_password.json()["detail"] == "LOGIN_BAD_CREDENTIALS"
+    assert response_bad_data.status_code == 422
 
 
-# @pytest.mark.asyncio
-# async def test_register(ac: AsyncClient):
-#     user_data = {
-#     "email": "pochtamy@bk.ru",
-#     "password": "password",
-#     "is_active": True,
-#     "is_superuser": False,
-#     "is_verified": False,
-#     "username": "rabotay",
-#     "lastname": "last",
-#     "firstname": "first"
-#     }
-#     response = await ac.post("/auth/register", json=user_data)
-#
-#     assert response.status_code == 201
 
 
